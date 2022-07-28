97f8ed6580b00c72e7b069b669c257c313c8a55b
lazarmijatovicra942018 <mijatovic.ra94.2018@uns.ac.rs>
2022-07-28 14:24:19 +0200

    pocetak pocetne i stranice logovanja

--- SportskiCentri/src/main/webapp/components/loginPage.js
+++ SportskiCentri/src/main/webapp/components/loginPage.js
@@ -3,11 +3,26 @@ Vue.component("login-page", {
 	template: ` 
 <div>
 	<div class="topnav">
- 	 <a  style="float: left; "href="#/">Početna strana</a>
- 	 <a  href="#/registration">Registruj se</a>
- 	 <a  class="active" href="#/login">Uloguj se</a>
+ 	 	<a  style="float: left; "href="#/">Početna strana</a>
+ 	 	<a  href="#/registration">Registruj se</a>
+ 	 	<a  class="active" href="#/login">Uloguj se</a>
  	 
 	</div>
+	
+	<div class="log">
+		<form @submit='login'>
+				<table>
+					<tr><td>Korisničko ime</td><td><input type="text"></td></tr>
+					<tr><td>Lozinka</td><td><input type="password" ></td></tr>
+					<tr><b><td><input type="submit" value="Uloguj se"></td></b></tr>
+					<tr><td id="logInText"></td></tr>
+				</table>
+			</form>
+	
+	</div>
+	
+	
+	
 </div>		  
 `
 	, 
